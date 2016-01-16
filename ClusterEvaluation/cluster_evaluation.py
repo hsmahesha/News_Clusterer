81a47bb94b5af01632a9b67b6d9c5c4495e0e3a6
mahesha-s <mahesha.shivamallappa@gmail.com>
2016-01-16 20:03:45 +0530

    added preliminary support to cluster evaluation

---
+++ ClusterEvaluation/cluster_evaluation.py
@@ -0,0 +1,68 @@
+import csv                                                                      
+import sys
+import numpy as np
+from sklearn import metrics
+
+def get_evaluation_score(labelArr, vecArr, metric):
+    eVal = metrics.silhouette_score(vecArr, labelArr, metric)
+    return eVal
+
+def read_cpp_file(iFile, numTerms):
+    iList = list(csv.reader(iFile, delimiter='\t'))
+    numVec = len(iList)
+    labelArr = np.zeros(numVec, int)
+    vecArr = np.zeros((numVec, numTerms))
+    curInd = 0
+    for iNode in iList:
+        labelArr[curInd] = int(iNode[0])
+        vStr = str(iNode[1])
+        vList = vStr.split(',')
+        tVec = np.zeros(numTerms)
+        for vNode in vList:
+            vNodeList = str(vNode).split(':')
+            tNo = int(vNodeList[0])
+            tVal = float(vNodeList[1])
+            tVec[tNo] = tVal
+        vecArr[curInd] = tVec[:]
+        curInd += 1
+    return labelArr, vecArr
+
+def open_in_file(fStr):
+    try:
+        iFile = open(fStr, "r")
+    except:
+        print("Failed to open the file " + fStr)
+        sys.exit()
+    return iFile
+
+def open_out_file():
+    fStr = './output/evaluation.txt'
+    try:
+        oFile = open(fStr, "w")
+    except:
+        print("Failed to open the file " + fStr)
+        sys.exit()
+    return oFile
+
+def evaluate_clusters():
+    numTermFileStr = './input/num_terms.txt'
+    numTermFile = open_in_file(numTermFileStr)
+    numTerms = int(numTermFile.readline())
+    numTermFile.close()
+
+    cppFileStr = './input/p_cpp.txt'
+    cppFile = open_in_file(cppFileStr)
+    labelArr, vecArr = read_cpp_file(cppFile, numTerms)
+    cppFile.close()
+
+    print (len(labelArr))
+    print (len(vecArr))
+
+    eVal = get_evaluation_score(labelArr, vecArr, 'euclidean')
+
+    oFile = open_out_file()
+    outStr = "Silhouettes Score = " + str(eVal) + "\n"
+    oFile.write(outStr)
+    oFile.close() 
+
+evaluate_clusters()
--- cluster.sh
+++ cluster.sh
@@ -22,7 +22,8 @@ fi
 export HADOOP_HOME=./hadoop-2.6.0
 export SOLR_HOME=./solr-undertow-1.5.0
 export NUTCH_HOME=./nutch-1.10
-export MAHOUT_HOME=./mahout-0.11.1                      
+export MAHOUT_HOME=./mahout-0.11.1         
+EVAL_HOME=./ClusterEvaluation
 
 # clean up all temp directories and logs in order to start from scratch
 rm -r ./news/hadoop/namenode/* > /dev/null 2>&1
@@ -33,11 +34,12 @@ rm -r ./news/solr/temp/* > /dev/null 2>&1
 rm -r ./news/nutch/crawl/* > /dev/null 2>&1
 rm -r ./news/nutch/urls/* > /dev/null 2>&1
 rm -r ./news/mahout/* > /dev/null 2>&1
-rm -r ./result/* > /dev/null 2>&1
 rm -r $HADOOP_HOME/logs/* > /dev/null 2>&1
 rm -r $SOLR_HOME/logs/* > /dev/null 2>&1
 rm -r $NUTCH_HOME/logs/* > /dev/null 2>&1
 rm -r $MAHOUT_HOME/logs/* > /dev/null 2>&1
+rm -r $EVAL_HOME/input/* >/dev/null 2>&1
+rm -r $EVAL_HOME/output/* >/dev/null 2>&1
 
 # set hadoop related environment variables.
 export HADOOP_PREFIX=$HADOOP_HOME
@@ -113,7 +115,7 @@ echo "http://www.latimes.com/" >> $seed_file 2>&1
 echo "Message: Started crawling and indexing of news articles"
 sleep 2
 $nutch inject $crawldb $urls
-for i in `seq 1 1`;
+for i in `seq 1 2`;
 do
    $nutch generate $crawldb $segments -topN 1000 
    s1=`ls -d $segments/2* | tail -1`
@@ -136,8 +138,7 @@ echo "Message: Indexing is done. Solr server is stopped."
 
 # final results are stored here
 res_dir=./result
-result_file=./$res_dir/result.txt
-touch result_file
+result_file=$res_dir/result.txt
 
 # mahout related variables
 MAHOUT_LOCAL=true
@@ -162,6 +163,10 @@ $mahout lucene.vector -d $index_dir -o $vec_file -t $dict_file \
 --idField id  -f text -n 2 -w TFIDF
 sleep 2
 echo "Message: Finished getting lucene vectors."
+
+# save dictionary file in result directory. Which helps to interpret the
+# solr/lucene selected terms.
+cp $dict_file $res_dir/
 
 sleep 2
 echo "Message: Clustering news articles using mahout clustering algorithm."
@@ -192,13 +197,47 @@ do
       count=`expr $count + 1`
    fi
 done < $cpp_list_file
+
+cpp_seq_file=$out_dir/clusteredPoints/part*
+cpp_txt_file=$out_dir/clusteredPoints/cpp.txt
+$mahout vectordump -i $cpp_seq_file -o $cpp_txt_file -p printKey
+
+p_cpp_txt_file=$out_dir/clusteredPoints/p_cpp.txt
+sed 's/{//g' <$cpp_txt_file | sed 's/}//g' >$p_cpp_txt_file
+
+final_seq_dir=""
+for D in $out_dir/clusters-*-final
+do
+  final_seq_dir=$D
+done
+final_txt_file=$final_seq_dir/final.txt
+$mahout clusterdump -i $final_seq_dir -o $final_txt_file -n 0
+
+p_final_txt_file=$final_seq_dir/p_final.txt
+sed 's/\",\"r/\"\t\"r/g' <$final_txt_file | sed -E 's/,\"n\":[0-9]+//g' | \
+sed 's/\t\"r\":\[*\],\"c\":/\t/g' | sed 's/\"identifier\":\"[CV]L-//g' | \
+sed 's/\"//g' | sed 's/{//g' | sed 's/}//g' | sed 's/\[//g' | sed 's/\]//g' \
+>$p_final_txt_file
 sleep 2
 echo "Message: Finished postprocessng of clustering result."
 
+sleep 2
+echo "Message: Evaluating the clustering result."
+sleep 2
+eval_in_dir=$EVAL_HOME/input
+eval_out_dir=$EVAL_HOME/output
+eval_out_file=$eval_out_dir/evaluation.txt
+final_eval_file=$res_dir/evaluation.txt
 
-# save dictionary file in result directory. Which helps to interpret the
-# solr/lucene selected terms.
-cp $dict_file $res_dir/
+cp $p_cpp_txt_file $eval_in_dir/
+cp $p_final_txt_file $eval_in_dir/
+head -1 $dict_file > $eval_in_dir/num_terms.txt
+cd $EVAL_HOME
+python cluster_evaluation.py
+cd ../ 
+cp $eval_out_file $final_eval_file 
+sleep 2
+echo "Message: Done with evaluating the clustering result."
 
 # clean up all temp directories and logs
 rm -r ./news/hadoop/namenode/* > /dev/null 2>&1
@@ -209,15 +248,18 @@ rm -r ./news/solr/temp/* > /dev/null 2>&1
 rm -r ./news/nutch/crawl/* > /dev/null 2>&1
 rm -r ./news/nutch/urls/* > /dev/null 2>&1
 rm -r ./news/mahout/* > /dev/null 2>&1
+rm -r ./temp/* > /dev/null 2>&1
 rm -r $HADOOP_HOME/logs/* > /dev/null 2>&1
 rm -r $SOLR_HOME/logs/* > /dev/null 2>&1
 rm -r $NUTCH_HOME/logs/* > /dev/null 2>&1
 rm -r $MAHOUT_HOME/logs/* > /dev/null 2>&1
-rm -r ./temp/* > /dev/null 2>&1
+rm -r $EVAL_HOME/input/* > /dev/null 2>&1
+rm -r $EVAL_HOME/output/* > /dev/null 2>&1
 
 # print final message
 echo -e ""
 echo "================================================================="
-echo "Done with the clustering of news articles. The final result is 
-available in the file $final_result_file"
+echo "Done with the clustering of news articles."
+echo "The final result is available in the file $final_result_file"
+echo "The evalaution result is available in the file $final_eval_file"
 echo "================================================================="
