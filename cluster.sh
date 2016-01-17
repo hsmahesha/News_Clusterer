6a2d4dfeecabb263ec81405f472a5b5fd625378a
mahesha-s <mahesha.shivamallappa@gmail.com>
2016-01-17 19:23:00 +0530

    support for more than one distance measure

--- cluster.sh
+++ cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#clear screen
+# clear console
 clear
 
 # check if java home environment variable is set
@@ -23,18 +23,24 @@ export HADOOP_HOME=./hadoop-2.6.0
 export SOLR_HOME=./solr-undertow-1.5.0
 export NUTCH_HOME=./nutch-1.10
 export MAHOUT_HOME=./mahout-0.11.1         
-EVAL_HOME=./ClusterEvaluation
+export EVAL_HOME=./ClusterEvaluation
+export RES_HOME=./result
+export NHD=./news/hadoop
+export NSD=./news/solr
+export NND=./news/nutch
+export NMD=./news/mahout
 
 # clean up all temp directories and logs in order to start from scratch
-rm -r ./news/hadoop/namenode/* > /dev/null 2>&1
-rm -r ./news/hadoop/datanode/* > /dev/null 2>&1
-rm -r ./news/solr/home/core1/data/* > /dev/null 2>&1
-rm -r ./news/solr/logs/* > /dev/null 2>&1
-rm -r ./news/solr/temp/* > /dev/null 2>&1
-rm -r ./news/nutch/crawl/* > /dev/null 2>&1
-rm -r ./news/nutch/urls/* > /dev/null 2>&1
-rm -r ./news/mahout/* > /dev/null 2>&1
-rm -r ./result/* > /dev/null 2>&1 
+rm -r $NHD/namenode/* > /dev/null 2>&1
+rm -r $NHD/datanode/* > /dev/null 2>&1
+rm -r $NSD/home/core1/data/* > /dev/null 2>&1
+rm -r $NSD/logs/* > /dev/null 2>&1
+rm -r $NSD/temp/* > /dev/null 2>&1
+rm -r $NND/crawl/* > /dev/null 2>&1
+rm -r $NND/urls/* > /dev/null 2>&1
+rm -r $NMD/* > /dev/null 2>&1
+rm -r $RES_HOME/tanimoto/* > /dev/null 2>&1
+rm -r $RES_HOME/cosine/* > /dev/null 2>&1
 rm -r $HADOOP_HOME/logs/* > /dev/null 2>&1
 rm -r $SOLR_HOME/logs/* > /dev/null 2>&1
 rm -r $NUTCH_HOME/logs/* > /dev/null 2>&1
@@ -54,16 +60,6 @@ export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
 
 # append hadoop bin and sbin to PATH
 export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
-
-# we have defined temporary hadoop namenode directory as /tmp/_hdtmp_ in 
-# the hadoop configuration file "core-site.xml". Hence create this directory if
-# it is not already created.
-hfsd="/tmp/_hdtmp_"
-if [ ! -d $hfsd ]
-then
-   mkdir $hfsd
-   chmod 777 $hfsd
-fi
 
 # set solr related variables
 solr_log=./temp/solr.log
@@ -93,10 +89,10 @@ sleep 2
 
 # nutch related variables
 nutch=$NUTCH_HOME/bin/nutch
-crawldb=./news/nutch/crawl/crawldb
-segments=./news/nutch/crawl/segments
-linkdb=./news/nutch/crawl/linkdb
-urls=./news/nutch/urls
+crawldb=$NND/crawl/crawldb
+segments=$NND/crawl/segments
+linkdb=$NND/crawl/linkdb
+urls=$NND/urls
 
 # create seed.txt file and add seed urls to it.
 seed_file=$urls/seed.txt
@@ -137,144 +133,157 @@ echo ""
 sleep 3 
 echo "Message: Indexing is done. Solr server is stopped."
 
-# final results are stored here
-res_dir=./result
-result_file=$res_dir/result.txt
-
 # mahout related variables
 MAHOUT_LOCAL=true
 mahout=$MAHOUT_HOME/bin/mahout
-index_dir=./news/solr/home/core1/data/index 
-in_dir=./news/mahout/input
-vec_seq_file=$in_dir/vector-00000
-vec_text_file=$in_dir/vector.txt
-dict_file=$in_dir/dictionary.txt
-num_vec_file=$in_dir/num_vecs.txt
-out_dir=./news/mahout/output
-seed_dir=./news/mahout/seed
-cpp_dir=./news/mahout/cpp
-max_iter=20
-distance_measure=org.apache.mahout.common.distance.TanimotoDistanceMeasure
-cpp_list_file=./news/mahout/cpp_list.txt
-count=1
+INDEX_DIR=$NSD/home/core1/data/index 
+IN_DIR=$NMD/input
+vec_seq_file=$IN_DIR/vector-00000
+vec_text_file=$IN_DIR/vector.txt
+dict_file=$IN_DIR/dictionary.txt
+num_vec_file=$IN_DIR/num_vecs.txt
 
 sleep 2
 echo "Message: Getting lucene vectors from solr index."
 sleep 2
-$mahout lucene.vector -d $index_dir -o $vec_seq_file -t $dict_file \
+$mahout lucene.vector -d $INDEX_DIR -o $vec_seq_file -t $dict_file \
 --idField id  -f text -n 2 -w TFIDF
 sleep 2
 echo "Message: Finished getting lucene vectors."
 
+# get the value of k as sqrt(n/2) where n is the number of vectors
 sleep 2
 echo "Message: Getting the total number of vectors."
-# save dictionary file in result directory. Which helps to interpret the
-# solr/lucene selected terms.
-cp $dict_file $res_dir/
-
-# get the total number of vectors
 $mahout vectordump -i $vec_seq_file -o $vec_text_file
-
-#get the value of k as sqrt(n/2) where n is the number of vectors
 cat $vec_text_file | wc -l > $num_vec_file
 let k=$(cat $num_vec_file)
 k=$(echo "sqrt($k/2)" | bc -l)
 let k=${k%.*}
+cp $dict_file $RES_HOME/
 sleep 2
-echo "Message: Finished etting the total number of vectors"
+echo "Message: Finished getting the total number of vectors"
 
-sleep 2
-echo "Message: Clustering news articles using mahout clustering algorithm."
-sleep 2
-$mahout kmeans -i $vec_seq_file -o $out_dir -c $seed_dir -k $k \
--x $max_iter -dm $distance_measure -cl -ow
-sleep 2
-echo "Message: Finished clustering of news articles."
+# similarity measures to be explored
+DIST_ARR=(cosine tanimoto)
 
-sleep 2                                                                         
-echo "Message: Postprocessng the clustering result."     
-sleep 2
-$mahout clusterpp -i $out_dir -o $cpp_dir -ow
-sleep 2
-ls -d1 $cpp_dir/* > $cpp_list_file 2>&1
-while read line
-do 
-   if [ -d $line ]
-   then
-      echo -e '===========================================================' >> $result_file
-      echo -e "Cluster $count" >> $result_file 
-      echo -e '===========================================================' >> $result_file
-      seq_file=$line/part*
-      $mahout vectordump -i $seq_file -o $line/dump.txt -d $dict_file -dt text -N nameOnly
-      cat $line/dump.txt >> $result_file
-      echo -e '===========================================================' >> $result_file
-      echo -e "" >> $result_file
-      count=`expr $count + 1`
-   fi
-done < $cpp_list_file
-
-cpp_seq_file=$out_dir/clusteredPoints/part*
-cpp_txt_file=$out_dir/clusteredPoints/cpp.txt
-$mahout vectordump -i $cpp_seq_file -o $cpp_txt_file -p printKey
-
-p_cpp_txt_file=$out_dir/clusteredPoints/p_cpp.txt
-sed 's/{//g' <$cpp_txt_file | sed 's/}//g' >$p_cpp_txt_file
-
-final_seq_dir=""
-for D in $out_dir/clusters-*-final
+# processs the data set for each distance measure
+for dist in ${DIST_ARR[@]}
 do
-  final_seq_dir=$D
+  # final cluster results are stored here
+  RES_DIR=$RES_HOME/$dist
+  result_file=$RES_DIR/result.txt
+
+  # create a directory to store result for current distance measure
+  mkdir $NMD/$dist
+
+  OUT_DIR=$NMD/$dist/output
+  SEED_DIR=$NMD/$dist/seed
+  max_iter=20
+
+  if [ "$dist" == "tanimoto" ]
+  then
+     distance_measure=org.apache.mahout.common.distance.TanimotoDistanceMeasure
+  elif [ "$dist" == "cosine" ]
+  then
+    distance_measure=org.apache.mahout.common.distance.CosineDistanceMeasure
+  fi
+  
+  sleep 2
+  echo "Message: Clustering news articles using mahout clustering algorithm."
+  sleep 2
+  $mahout kmeans -i $vec_seq_file -o $OUT_DIR -c $SEED_DIR -k $k \
+  -x $max_iter -dm $distance_measure -cl -ow
+  sleep 2
+  echo "Message: Finished clustering of news articles."
+
+  CPP_DIR=$NMD/$dist/cpp
+  cpp_list_file=$NMD/$dist/cpp_list.txt
+  count=1
+
+  sleep 2                                                                         
+  echo "Message: Dumping each cluster in separate directory"     
+  sleep 2
+  $mahout clusterpp -i $OUT_DIR -o $CPP_DIR -ow
+  sleep 2
+  ls -d1 $CPP_DIR/* > $cpp_list_file 2>&1
+  while read line
+  do 
+     if [ -d $line ]
+     then
+        echo -e '===========================================================' >> $result_file
+        echo -e "Cluster $count" >> $result_file 
+        echo -e '===========================================================' >> $result_file
+        seq_file=$line/part*
+        $mahout vectordump -i $seq_file -o $line/dump.txt -d $dict_file -dt text -N nameOnly
+        cat $line/dump.txt >> $result_file
+        echo -e '===========================================================' >> $result_file
+        echo -e "" >> $result_file
+        count=`expr $count + 1`
+     fi
+  done < $cpp_list_file
+  sleep 2
+  echo "Message: Finished dumping of clusters"
+
+  sleep 2
+  echo "Message: Postprocessing clustering result"
+  sleep 2
+  cpp_seq_file=$OUT_DIR/clusteredPoints/part*
+  cpp_txt_file=$OUT_DIR/clusteredPoints/cpp.txt
+  p_cpp_txt_file=$OUT_DIR/clusteredPoints/p_cpp.txt
+  $mahout vectordump -i $cpp_seq_file -o $cpp_txt_file -p printKey
+  sed 's/{//g' <$cpp_txt_file | sed 's/}//g' >$p_cpp_txt_file
+
+  FSD=""
+  for D in $OUT_DIR/clusters-*-final
+  do
+    FSD=$D
+  done
+  final_txt_file=$FSD/final.txt
+  p_final_txt_file=$FSD/p_final.txt
+  $mahout clusterdump -i $FSD -o $final_txt_file -n 0
+  sed 's/\",\"r/\"\t\"r/g' <$final_txt_file | sed -E 's/,\"n\":[0-9]+//g' | \
+  sed 's/\t\"r\":\[*\],\"c\":/\t/g' | sed 's/\"identifier\":\"[CV]L-//g' | \
+  sed 's/\"//g' | sed 's/{//g' | sed 's/}//g' | sed 's/\[//g' | sed 's/\]//g' \
+  >$p_final_txt_file
+  sleep 2
+  echo "Message: Finished postprocessng of clustering result."
+
+  sleep 2
+  echo "Message: Evaluating the clustering result."
+  sleep 2
+  EID=$EVAL_HOME/input
+  cp $p_cpp_txt_file $EID/
+  cp $p_final_txt_file $EID/
+  head -1 $dict_file > $EID/num_terms.txt
+  cd $EVAL_HOME
+  python cluster_evaluation.py
+  cd ../ 
+  EOD=$EVAL_HOME/output
+  cp $EOD/evaluation.txt $RES_DIR/
+  sleep 2
+  echo "Message: Done with evaluating the clustering result."
+
 done
-final_txt_file=$final_seq_dir/final.txt
-$mahout clusterdump -i $final_seq_dir -o $final_txt_file -n 0
 
-p_final_txt_file=$final_seq_dir/p_final.txt
-sed 's/\",\"r/\"\t\"r/g' <$final_txt_file | sed -E 's/,\"n\":[0-9]+//g' | \
-sed 's/\t\"r\":\[*\],\"c\":/\t/g' | sed 's/\"identifier\":\"[CV]L-//g' | \
-sed 's/\"//g' | sed 's/{//g' | sed 's/}//g' | sed 's/\[//g' | sed 's/\]//g' \
->$p_final_txt_file
-sleep 2
-echo "Message: Finished postprocessng of clustering result."
-
-sleep 2
-echo "Message: Evaluating the clustering result."
-sleep 2
-eval_in_dir=$EVAL_HOME/input
-eval_out_dir=$EVAL_HOME/output
-eval_out_file=$eval_out_dir/evaluation.txt
-final_eval_file=$res_dir/evaluation.txt
-
-cp $p_cpp_txt_file $eval_in_dir/
-cp $p_final_txt_file $eval_in_dir/
-head -1 $dict_file > $eval_in_dir/num_terms.txt
-cd $EVAL_HOME
-python cluster_evaluation.py
-cd ../ 
-cp $eval_out_file $final_eval_file 
-sleep 2
-echo "Message: Done with evaluating the clustering result."
-
-# clean up all temp directories and logs
-rm -r ./news/hadoop/namenode/* > /dev/null 2>&1
-rm -r ./news/hadoop/datanode/* > /dev/null 2>&1
-rm -r ./news/solr/home/core1/data/* > /dev/null 2>&1
-rm -r ./news/solr/logs/* > /dev/null 2>&1
-rm -r ./news/solr/temp/* > /dev/null 2>&1
-rm -r ./news/nutch/crawl/* > /dev/null 2>&1
-rm -r ./news/nutch/urls/* > /dev/null 2>&1
-rm -r ./news/mahout/* > /dev/null 2>&1
-rm -r ./temp/* > /dev/null 2>&1
+rm -r $NHD/namenode/* > /dev/null 2>&1
+rm -r $NHD/datanode/* > /dev/null 2>&1
+rm -r $NSD/home/core1/data/* > /dev/null 2>&1
+rm -r $NSD/logs/* > /dev/null 2>&1
+rm -r $NSD/temp/* > /dev/null 2>&1
+rm -r $NND/crawl/* > /dev/null 2>&1
+rm -r $NND/urls/* > /dev/null 2>&1
+rm -r $NMD/* > /dev/null 2>&1
 rm -r $HADOOP_HOME/logs/* > /dev/null 2>&1
 rm -r $SOLR_HOME/logs/* > /dev/null 2>&1
 rm -r $NUTCH_HOME/logs/* > /dev/null 2>&1
 rm -r $MAHOUT_HOME/logs/* > /dev/null 2>&1
-rm -r $EVAL_HOME/input/* > /dev/null 2>&1
-rm -r $EVAL_HOME/output/* > /dev/null 2>&1
+rm -r $EVAL_HOME/input/* >/dev/null 2>&1
+rm -r $EVAL_HOME/output/* >/dev/null 2>&1
+rm -r ./temp/* > /dev/null 2>&1
 
 # print final message
 echo -e ""
-echo "================================================================="
-echo "Done with the clustering of news articles."
-echo "The final result is available in the file $final_result_file"
-echo "The evalaution result is available in the file $final_eval_file"
-echo "================================================================="
+echo "========================================================================"
+echo "Done with the clustering of news articles. The result is available in the
+directory ${RES_HOME}."
+echo "========================================================================"
